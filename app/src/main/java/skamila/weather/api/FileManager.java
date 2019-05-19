0b804ac8396c1a21cee1bd4d2031160ca0b03877
Kamila <kami.wiewiora@gmail.com>
2019-05-19 18:14:21 +0200

    dodanie klasy FileManager

---
+++ app/src/main/java/skamila/weather/api/FileManager.java
@@ -0,0 +1,71 @@
+package skamila.weather.api;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class FileManager {
+
+    private Context context;
+    private String fileName;
+
+    public FileManager(Context context, String fileName){
+        this.context = context;
+        this.fileName = fileName;
+    }
+
+    public void saveToFile(String text) {
+
+        FileOutputStream fos;
+
+        try {
+            fos = context.openFileOutput(fileName, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String loadFromFile() {
+
+        FileInputStream fis;
+        String text;
+
+        try {
+            fis = context.openFileInput(fileName);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+
+            while ((text = br.readLine()) != null) {
+                sb.append(text);
+            }
+
+            fis.close();
+            isr.close();
+            br.close();
+
+            return sb.toString();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+
+    }
+
+}
