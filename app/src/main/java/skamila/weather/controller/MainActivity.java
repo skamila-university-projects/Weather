23dbad68bbe79bd93c62a41f34444781fe515c18
Kamila <kami.wiewiora@gmail.com>
2019-06-06 21:45:32 +0200

    dodanie metody sprawdzającej połączenie z Internetem

--- app/src/main/java/skamila/weather/controller/MainActivity.java
+++ app/src/main/java/skamila/weather/controller/MainActivity.java
@@ -1,34 +1,42 @@
 package skamila.weather.controller;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import skamila.weather.R;
 import skamila.weather.api.ApiClient;
 import skamila.weather.api.ApiController;
 import skamila.weather.api.FileManager;
-import skamila.weather.api.forecast.Forecast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        final TextView textView = findViewById(R.id.text);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        if(!isInternetConnection()){
+            Toast.makeText(this, "No connection to the Internet. \n" +
+                    "Data may be out of date.", Toast.LENGTH_LONG).show();
+        }
 
         final FileManager fileManager = new FileManager(this, "data.txt");
 
         ApiClient apiClient = new ApiClient();
         ApiController apiController = new ApiController(this, apiClient, fileManager);
-        String data = apiController.downloadForecast("Poznan", "pl");
+        String data = apiController.downloadForecast("Lodz", "pl");
 
+    }
 
-        textView.setText(fileManager.loadFromFile());
-        Forecast forecast = ApiController.convertForecastToObject(fileManager.loadFromFile());
-
+    private boolean isInternetConnection() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 
 }
